04947e58b474492c78b9d0d8fc2673a7124ce968
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-05 09:38:11 +0300

    Add instance methods to Dog and Person classes

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+# Testing the methods
+if __name__ == "__main__":
+    fido = Dog()
+    fido.bark()  # Should print "Woof!"
+    fido.sit()   # Should print "The dog is sitting."
+    
+    snoopy = Dog()
+    snoopy.bark()  # Should print "Woof!"
+    snoopy.sit()   # Should print "The dog is sitting."
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+# Testing the methods
+if __name__ == "__main__":
+    john = Person()
+    john.talk()  # Should print "Hello World!"
+    john.walk()  # Should print "The person is walking."
+    
+    jane = Person()
+    jane.talk()  # Should print "Hello World!"
+    jane.walk()  # Should print "The person is walking."
